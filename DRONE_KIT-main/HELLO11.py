73dd7c6824a94f874665ecfee9768ef8928bbf06
AjinkyaBagmar <73771478+AjinkyaBagmar@users.noreply.github.com>
2021-05-28 10:13:11 +0530

    Update HELLO11.py

--- DRONE_KIT-main/HELLO11.py
+++ DRONE_KIT-main/HELLO11.py
@@ -1,10 +1,9 @@
 from dronekit import connect, VehicleMode
 
-# Connect to the Vehicle.
 print("Connecting to vehicle on")
 vehicle = connect("udp:127.0.0.1:14550", wait_ready=True)
 
-# Get some vehicle attributes (state)
+
 print ("Get some vehicle attribute values:")
 print (" GPS: %s" % vehicle.gps_0)
 print (" Battery: %s" % vehicle.battery)
@@ -13,6 +12,5 @@ print (" Is Armable?: %s" % vehicle.is_armable)
 print (" System status: %s" % vehicle.system_status.state)
 print (" Mode: %s" % vehicle.mode.name)    # settable
 
-# Close vehicle object before exiting script
 vehicle.close()
 print("Completed")
